9dd6903e067b94158c4de7effbd003090e6adf99
Grey-kingreys <soulmamoudou0@gmail.com>
2025-06-27 16:26:58 +0000

    Mise a jour

--- js/script.js
+++ js/script.js
@@ -75,13 +75,3 @@ window.addEventListener('scroll', function() {
         }
     });
 });
-
-// Form submission (example)
-// const form = document.querySelector('form');
-// if (form) {
-//     form.addEventListener('submit', function(e) {
-//         e.preventDefault();
-//         alert('Merci pour votre message! Je vous répondrai dès que possible.');
-//         form.reset();
-//     });
-// }
